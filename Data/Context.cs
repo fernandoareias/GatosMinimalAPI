fdbd26cd5793a2b7ed6f2ef85195f2c77ba967d0
fernandoareias <nando.calheirosx@gmail.com>
2021-10-10 17:49:13 -0300

    Context

---
+++ Data/Context.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GatosMinimal.API.Data
+{
+    public class Context : DbContext
+    {
+        public DbSet<Gato> Gatos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options) 
+            => options.UseSqlServer("SUA CONNECTION STRING :)");
+    }
+}
